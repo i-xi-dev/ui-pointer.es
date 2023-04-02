36790509367f3fbcb343cfdf902b125c42c955f0
i-xi-dev <naoyuki.isono@gmail.com>
2023-04-02 22:38:25 +0900

    in development

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { type PointerActivity, type PointerTrace, Pointer } from "./pointer";
 export { PointerObserver } from "./pointer_observer";
 
 //TODO readmeに記載↓
-
 // - ターゲットのboundingBox外に位置する子孫の扱い
 //   Trackingは開始する仕様とする
 //   - ターゲットの子孫が何らかのCSS(position:absoluteなど)で、ターゲットのboundingBoxの外にある時
@@ -11,16 +10,14 @@ export { PointerObserver } from "./pointer_observer";
 // - 前提条件として
 //   - windowとtargetでlistenしているので、到達前にpointereventをキャンセルされたら検知できなくなる
 //   - touch-actionはブロックに適用
-//   - その他box数が1ではない場合
-//     - 0: displayがnone,contents,...
-//     - 1以上: displayがinline,...
-//     - 通常は1だが2以上になることがある: page-break以外のbreak (regionは廃止されたのでcolumnだけか？) //TODO これは重大な問題な気がしてきた targetX/Yは一旦消すか？
+//   - display算出値がinlineの場合、正常動作しない（座標系がブラウザによって異なる）
+//     以下の場合もおそらく問題ある
+//     - ボックスが0: displayがnone,contents,...
+//     - ボックスが2以上: column-break等のbreak (regionは廃止され、screenでpage-breakするブラウザはなくなったので、column-breakだけか) 
 //   - 非trustedなPointerEventは無条件で無視している
 //   - 異なるpointerTypeのポインターを同時操作できるかはブラウザに依存
-
 // readmeに記載↑
 
 // 検討↓
-
 // - タッチの総体やwheelも扱う版
 //     wheelでスクロールしたときブラウザによってpointerenter,pointerleaveが発火しない
